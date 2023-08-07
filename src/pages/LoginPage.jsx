f4830e6ae56a39208ef569f1738c07f1d3b3a93e
madyanes <madyanes.dev@gmail.com>
2023-08-07 23:42:22 +0700

    refactor: delete unneeded components

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,17 @@
-import Login from '../components/Login'
-
 const LoginPage = () => {
-  return (<Login />)
+  return (
+    <article className="auth-form">
+      <h1>Login Form</h1>
+      <form action="#">
+        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Password" />
+        <div className="auth-buttons">
+          <button className="register">Login</button>
+          <p className="login"><a href="#">Don&apos;t have an account? Register here</a></p>
+        </div>
+      </form>
+    </article>
+  )
 }
 
 export default LoginPage
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,7 +1,19 @@
-import Register from '../components/Register'
-
 const RegisterPage = () => {
-  return (<Register />)
+  return (
+    <article className="auth-form">
+      <h1>Registration Form</h1>
+      <form action="#">
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Password" />
+        <input type="text" placeholder="Confirm Password" />
+        <div className="auth-buttons">
+          <button className="register">Register</button>
+          <p className="login"><a href="#">Already have an account? Login here</a></p>
+        </div>
+      </form>
+    </article>
+  )
 }
 
 export default RegisterPage
