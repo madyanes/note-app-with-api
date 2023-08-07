095a8c3486595bb71cc9bc55c977dbdef878febf
madyanes <madyanes.dev@gmail.com>
2023-08-07 22:12:48 +0700

    basic layout styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import './assets/style/App.css'
 
 const App = () => {
-  return (<h1>Hello World</h1>)
+  return (
+    <>
+      <header className="site-header">
+        <h1>&lt; NoteApp &#47;&gt;</h1>
+        <nav className="site-navigation">
+          <ul>
+            <li><a href="#">Register</a></li>
+            <li><a href="#">Login</a></li>
+          </ul>
+        </nav>
+      </header>
+
+      <main></main>
+    </>
+  )
 }
 
 export default App
