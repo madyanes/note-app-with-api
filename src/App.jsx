91ea74a2c059c8372c5090c8a924492227faa79c
madyanes <madyanes.dev@gmail.com>
2023-08-07 22:57:46 +0700

    create registration page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import RegisterPage from './pages/RegisterPage'
 import './assets/style/App.css'
 
 const App = () => {
@@ -7,13 +9,17 @@ const App = () => {
         <h1>&lt; NoteApp &#47;&gt;</h1>
         <nav className="site-navigation">
           <ul>
-            <li><a href="#">Register</a></li>
+            <li><Link to='/register'>Register</Link></li>
             <li><a href="#">Login</a></li>
           </ul>
         </nav>
       </header>
 
-      <main></main>
+      <main>
+        <Routes>
+          <Route path='/register' element={<RegisterPage />} />
+        </Routes>
+      </main>
     </>
   )
 }
