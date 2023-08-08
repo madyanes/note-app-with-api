8f6aa27f8f9d702e1f5a90cbffee2b12c686d024
madyanes <madyanes.dev@gmail.com>
2023-08-08 07:07:39 +0700

    make login request

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,40 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { login } from "../utils/network-data"
+
 const LoginPage = () => {
+  const [userEmail, setUserEmail] = useState('')
+  const [userPassword, setUserPassword] = useState('')
+  const navigate = useNavigate()
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault()
+
+    const { error, data } = await login({
+      email: userEmail,
+      password: userPassword,
+    })
+
+    if (!error) {
+      navigate('/')
+      localStorage.setItem('accessToken', data.accessToken)
+    }
+  }
+
+  const onUserEmailChangeHandler = (event) => {
+    setUserEmail(() => event.target.value)
+  }
+
+  const onUserPasswordChangeHandler = (event) => {
+    setUserPassword(() => event.target.value)
+  }
+
   return (
     <article className="auth-form">
       <h1>Login Form</h1>
-      <form action="#">
-        <input type="text" placeholder="Email" />
-        <input type="text" placeholder="Password" />
+      <form onSubmit={onSubmitHandler}>
+        <input type="text" placeholder="Email" value={userEmail} onChange={onUserEmailChangeHandler} />
+        <input type="text" placeholder="Password" value={userPassword} onChange={onUserPasswordChangeHandler} />
         <div className="auth-buttons">
           <button className="register">Login</button>
           <p className="login"><a href="#">Don&apos;t have an account? Register here</a></p>
