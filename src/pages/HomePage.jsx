2266d546cdebd6e2732e833a042f5cc1dc2224e5
madyanes <madyanes.dev@gmail.com>
2023-08-09 04:50:11 +0700

    prevent unauthenticated user to access home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,25 @@
-import { useContext } from "react"
-import LoginPage from "../pages/LoginPage"
+import { useContext, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 import ActiveNotes from "../components/ActiveNotes"
 import AuthUserContext from "../contexts/AuthUserContext"
 
 const HomePage = () => {
   const { user } = useContext(AuthUserContext)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    console.log('homepage', user)
+    if (user === null) {
+      navigate('/login')
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user])
 
   return (
     <>
       {
         user === null ? (
-          <LoginPage />
+          <p>Loading...</p>
         ) : (
           <section className="note-list">
             <h1>Active Notes</h1>
