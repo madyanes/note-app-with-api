5f63d3e4b7575441d6860883fc98def5f9c25525
madyanes <madyanes.dev@gmail.com>
2023-08-09 02:10:56 +0700

    preventing logged in user to access <RegisterPage/>

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { register } from '../utils/network-data'
+import AuthUserContext from '../contexts/AuthUserContext'
 
 const RegisterPage = () => {
   const [userName, setUserName] = React.useState('')
   const [userEmail, setUserEmail] = React.useState('')
   const [userPassword, setUserPassword] = React.useState('')
   const [userConfirmPassword, setUserConfirmPassword] = React.useState('')
+  const user = React.useContext(AuthUserContext)
   const navigate = useNavigate()
+
+  useEffect(() => {
+    if (user !== null) {
+      navigate('/')
+    }
+  })
 
   const onRegisterHandler = async (event) => {
     event.preventDefault()
@@ -40,19 +48,27 @@ const RegisterPage = () => {
   }
 
   return (
-    <article className="auth-form">
-      <h1>Registration Form</h1>
-      <form onSubmit={onRegisterHandler}>
-        <input type="text" placeholder="Name" value={userName} onChange={onUserNameChangeHandler} />
-        <input type="text" placeholder="Email" value={userEmail} onChange={onUserEmailChangeHandler} />
-        <input type="text" placeholder="Password" value={userPassword} onChange={onUserPasswordChangeHandler} />
-        <input type="text" placeholder="Confirm Password" value={userConfirmPassword} onChange={onUserConfirmPasswordHandler} />
-        <div className="auth-buttons">
-          <button className="register">Register</button>
-          <p className="login"><a href="#">Already have an account? Login here</a></p>
-        </div>
-      </form>
-    </article>
+    <>
+      {
+        user !== null ? (
+          <h1>You&apos;ve been logged in. Redirecting to homepage.</h1>
+        ) : (
+          <article className="auth-form">
+            <h1>Registration Form</h1>
+            <form onSubmit={onRegisterHandler}>
+              <input type="text" placeholder="Name" value={userName} onChange={onUserNameChangeHandler} />
+              <input type="text" placeholder="Email" value={userEmail} onChange={onUserEmailChangeHandler} />
+              <input type="text" placeholder="Password" value={userPassword} onChange={onUserPasswordChangeHandler} />
+              <input type="text" placeholder="Confirm Password" value={userConfirmPassword} onChange={onUserConfirmPasswordHandler} />
+              <div className="auth-buttons">
+                <button className="register">Register</button>
+                <p className="login"><a href="#">Already have an account? Login here</a></p>
+              </div>
+            </form>
+          </article>
+        )
+      }
+    </>
   )
 }
 
