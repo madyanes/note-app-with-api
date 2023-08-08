338a85c67d9008bdca9071e34d95572747040fbb
madyanes <madyanes.dev@gmail.com>
2023-08-09 06:56:57 +0700

    fix login form glitch when user already login

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,14 @@ import './assets/style/App.css'
 
 const App = () => {
   const [user, setUser] = useState(null)
+  const [initializing, setInitializing] = useState(true)
   const navigate = useNavigate()
 
   useEffect(() => {
     const getLeggedInUser = async () => {
       const { data } = await getUserLogged()
       setUser(() => data)
+      setInitializing(() => false)
     }
 
     getLeggedInUser()
@@ -39,24 +41,28 @@ const App = () => {
     navigate('/')
   }
 
-  return (
-    <AuthUserContext.Provider value={authUserContextValue}>
-      <header className="site-header">
-        <Link to='/'>
-          <h1>&lt;NoteApp &#47;&gt;</h1>
-        </Link>
-        <Navigation />
-      </header>
+  if (initializing) {
+    return null
+  } else {
+    return (
+      <AuthUserContext.Provider value={authUserContextValue}>
+        <header className="site-header">
+          <Link to='/'>
+            <h1>&lt;NoteApp &#47;&gt;</h1>
+          </Link>
+          <Navigation />
+        </header>
 
-      <main>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route path='/login' element={<LoginPage loginSuccess={onLoginSuccess} />} />
-        </Routes>
-      </main>
-    </AuthUserContext.Provider>
-  )
+        <main>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/register' element={<RegisterPage />} />
+            <Route path='/login' element={<LoginPage loginSuccess={onLoginSuccess} />} />
+          </Routes>
+        </main>
+      </AuthUserContext.Provider>
+    )
+  }
 }
 
 export default App
