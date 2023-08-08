7526635a5de11cdf9aa3ce2df24e7d31b424aa58
madyanes <madyanes.dev@gmail.com>
2023-08-09 01:34:46 +0700

    store logged in user data to AuthUserContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 import { getUserLogged, putAccessToken } from './utils/network-data'
+import AuthUserContext from './contexts/AuthUserContext'
 import RegisterPage from './pages/RegisterPage'
 import LoginPage from './pages/LoginPage'
 import HomePage from './pages/HomePage'
@@ -15,6 +16,12 @@ const App = () => {
     console.log(user)
   }, [user])
 
+  const authUserContextValue = useMemo(() => {
+    return {
+      user,
+    }
+  }, [user])
+
   const onLoginSuccess = async ({ accessToken }) => {
     putAccessToken(accessToken)
     const { data } = await getUserLogged()
@@ -23,7 +30,7 @@ const App = () => {
   }
 
   return (
-    <>
+    <AuthUserContext.Provider value={authUserContextValue}>
       <header className="site-header">
         <Link to='/'>
           <h1>&lt;NoteApp &#47;&gt;</h1>
@@ -38,7 +45,7 @@ const App = () => {
           <Route path='/login' element={<LoginPage loginSuccess={onLoginSuccess} />} />
         </Routes>
       </main>
-    </>
+    </AuthUserContext.Provider>
   )
 }
 
