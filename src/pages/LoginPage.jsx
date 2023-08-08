592ac1d2ef8f19be9ede5957cb7b294c1594d5de
madyanes <madyanes.dev@gmail.com>
2023-08-09 02:06:03 +0700

    prevent logged in user accessing <LoginPage/>

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,20 @@
-import { useState } from "react"
+import { useState, useContext, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 import PropTypes from 'prop-types'
 import { login } from "../utils/network-data"
+import AuthUserContext from "../contexts/AuthUserContext"
 
 const LoginPage = ({ loginSuccess }) => {
+  const navigate = useNavigate()
+  const { user } = useContext(AuthUserContext)
   const [userEmail, setUserEmail] = useState('')
   const [userPassword, setUserPassword] = useState('')
+
+  useEffect(() => {
+    if (user !== null) {
+      navigate('/')
+    }
+  })
 
   const onSubmitHandler = async (event) => {
     event.preventDefault()
@@ -28,17 +38,25 @@ const LoginPage = ({ loginSuccess }) => {
   }
 
   return (
-    <article className="auth-form">
-      <h1>Login Form</h1>
-      <form onSubmit={onSubmitHandler}>
-        <input type="text" placeholder="Email" value={userEmail} onChange={onUserEmailChangeHandler} />
-        <input type="text" placeholder="Password" value={userPassword} onChange={onUserPasswordChangeHandler} />
-        <div className="auth-buttons">
-          <button className="register">Login</button>
-          <p className="login"><a href="#">Don&apos;t have an account? Register here</a></p>
-        </div>
-      </form>
-    </article>
+    <>
+      {
+        user !== null ? (
+          <h1>You&apos;ve been logged in. Redirecting to homepage.</h1>
+        ) : (
+          <article className="auth-form">
+            <h1>Login Form</h1>
+            <form onSubmit={onSubmitHandler}>
+              <input type="text" placeholder="Email" value={userEmail} onChange={onUserEmailChangeHandler} />
+              <input type="text" placeholder="Password" value={userPassword} onChange={onUserPasswordChangeHandler} />
+              <div className="auth-buttons">
+                <button className="register">Login</button>
+                <p className="login"><a href="#">Don&apos;t have an account? Register here</a></p>
+              </div>
+            </form>
+          </article>
+        )
+      }
+    </>
   )
 }
 
