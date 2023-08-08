47b5c81c34760396398bf7cee3ae93e98298370f
madyanes <madyanes.dev@gmail.com>
2023-08-09 02:48:29 +0700

    show different navigation for authenticated user

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,33 @@
-import { Link } from 'react-router-dom'
+import { useContext, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import AuthUserContext from '../contexts/AuthUserContext'
 
 const Navigation = () => {
+  const { user } = useContext(AuthUserContext)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    console.log(user)
+    if (user === null) {
+      navigate('/login')
+    }
+  }, [user])
+
   return (
     <nav className="site-navigation">
-      <ul>
-        <li><Link to='/register'>Register</Link></li>
-        <li><Link to='/login'>Login</Link></li>
-      </ul>
+      {
+        user === null ? (
+          <ul>
+            <li><Link to='/register'>Register</Link></li>
+            <li><Link to='/login'>Login</Link></li>
+          </ul>
+        ) : (
+          <ul>
+            <li><Link to='/notes'>Notes</Link></li>
+            <li><Link to='/archives'>Archives</Link></li>
+          </ul>
+        )
+      }
     </nav>
   )
 }
