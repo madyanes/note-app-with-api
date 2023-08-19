c1cdd7d46d5060bbf25ddba3fb8c8f2a01b27a80
madyanes <madyanes.dev@gmail.com>
2023-08-19 07:58:44 +0700

    Display ActionButtons in DetailPage only

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,10 +1,19 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation, matchPath } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import showFormattedDate from '../utils/date-conversion'
+import ActionButtons from './ActionButtons'
 
 const NoteItem = ({ note }) => {
+  const { pathname } = useLocation()
+
+  // periksa apakah komponen NoteItem sedang berada di dalam komponen DetailPage atau tidak
+  const isInDetailPage = () => {
+    return matchPath('/notes/:id', pathname) ? true : false
+  }
+
   return (
     <article className='note-item'>
+      {isInDetailPage() ? <ActionButtons note={note} /> : <></>}
       <header>
         <Link to={`/notes/${note.id}`}>
           <h1>{note.title}</h1>
