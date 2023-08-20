756f534c77462763d2d7a27f40bc624040cdbc47
madyanes <madyanes.dev@gmail.com>
2023-08-20 15:58:52 +0700

    styling new note form

--- src/pages/NewNote.jsx
+++ src/pages/NewNote.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { BsFillCheckCircleFill } from 'react-icons/bs'
 import { addNote } from '../utils/network-data'
 
 const NewNote = () => {
@@ -10,17 +11,16 @@ const NewNote = () => {
   const onTitleChangeHandler = (event) => setNoteTitle(event.target.value)
   const onBodyInputHandler = (event) => setNoteBody(event.target.innerHTML)
 
-  const onSubmitHandler = (event) => {
-    event.preventDefault()
+  const onSubmitHandler = () => {
     addNote({ title: noteTitle, body: noteBody })
     navigate('/')
   }
 
   return (
-    <form onSubmit={onSubmitHandler}>
-      <input type="text" value={noteTitle} onChange={onTitleChangeHandler} />
-      <div contentEditable onInput={onBodyInputHandler} />
-      <button>Submit</button>
+    <form>
+      <input placeholder='Put the title of your note.' className='input-new-note-title' type="text" value={noteTitle} onChange={onTitleChangeHandler} />
+      <div contentEditable data-placeholder='What do you want to write about?' className='input-new-note-body' onInput={onBodyInputHandler} />
+      <BsFillCheckCircleFill className='fixed-button btn-icons' onClick={onSubmitHandler} />
     </form>
   )
 }
