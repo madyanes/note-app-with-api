5149f2a0b2e135bd96a699b3ece9017f60009fe2
madyanes <madyanes.dev@gmail.com>
2023-08-20 15:41:27 +0700

    display search placeholder based on locale setting

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,14 @@
 import { useContext } from 'react'
 import SearchContext from '../contexts/SearchContext'
+import LocaleContext from '../contexts/LocaleContext'
 
 const SearchBar = () => {
   const { keyword, onSearchChangeHandler } = useContext(SearchContext)
+  const { getTextLocale } = useContext(LocaleContext)
 
   return (
     <div className="search-bar">
-      <input type="text" placeholder="Search note..." value={keyword} onChange={(e) => onSearchChangeHandler(e.target.value)} />
+      <input type="text" placeholder={getTextLocale('search note...', 'cari catatan...')} value={keyword} onChange={(e) => onSearchChangeHandler(e.target.value)} />
     </div>
   )
 }
